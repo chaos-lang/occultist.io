4296f6e50cdaa63f5b22a6f4ce9f114303be9ba1
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-04-16 09:39:42 +0300

    Add curl effect for Browse the Library link and add GitHub corner

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import './styles.css'
 import './text-animation.scss'
+import './curl.css'
+import './github-corner.css'
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
